46e0c8647e7fbb3831fb2c031f8e3eb23fbaca85
César Arguijo <cesar.arguijo@vzert.com>
2022-06-10 11:51:19 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import "../plugins/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../plugins/jarallax/jarallax.js";
+import "../plugins/purecounterjs/dist/purecounter_vanilla.js";
+import "../plugins/glightbox/js/glightbox.js";
+import "../plugins/tiny-slider/tiny-slider.js";
+import "../plugins/aos/aos.js";
